5d88a19f572720447d84b3f4bbdb9fdff8836a5b
“ncpollock” <noah.pollock@yahoo.com>
2019-05-11 16:56:17 -0400

    put row input in sticky bar

--- ui.R
+++ ui.R
@@ -13,19 +13,35 @@ shinyUI(
                  'Generate Data', icon = icon("sliders")
                  , fluidRow(
                    useShinydashboard()
-                   , infoBoxOutput('df_columns')
-                   , infoBoxOutput('df_rows')
-                   , infoBoxOutput('df_size')
-                   , column(12,id="GlobalTools",class="sticky"
-                            # ,style="text-align: center;"
-                            , actionButton("add","Add New Variable",width = "40%"
-                                          ,style="background-color: green;"
-                                          ,icon = icon("plus-square"))
-                            , actionButton("preview","Preview Data",width = "40%"
-                                          # ,style="background-color: green;"
-                                          ,icon = icon("binoculars"))
-                            , downloadButton("downloadData","Download",style="float:right;"))
-                   , column(12,id = "var_header",column(3,"Variable Name"),column(4,"Type"),column(4,"Options / Details"))
+                   # , infoBoxOutput('df_columns')
+                   # , infoBoxOutput('df_rows')
+                   # , infoBoxOutput('df_size')
+                 )
+                 , fluidRow(id="GlobalTools",class="sticky"
+                            , column(3,actionButton("add","Add New Variable"
+                                                                     ,width = "100%"
+                                                                     ,style="background-color: green;"
+                                                                     ,icon = icon("plus-square")))
+                                  , column(3,actionButton("preview","Preview Data"
+                                                 ,width = "100%"
+                                                 ,icon = icon("binoculars")))
+                            # , column(4, infoBoxOutput('df_rows'))
+                            , column(1, p(icon("align-justify"),"Rows:",style = "font-size: 20px;text-align:right;padding-top:10px;")),column(2,sliderInput("df_rows",label=NA,1,1000,value=100,ticks = FALSE))
+                            , column(3,downloadButton("downloadData","Download",style="float:right;"))
+                   # , column(12,id="GlobalTools",class="sticky"
+                   #          # ,style="text-align: center;"
+                   #          , div(style = "float:left;",actionButton("add","Add New Variable"
+                   #                         # ,width = "50%"
+                   #                        ,style="background-color: green;"
+                   #                        ,icon = icon("plus-square"))
+                   #          , actionButton("preview","Preview Data"
+                   #                         # ,width = "50%"
+                   #                        # ,style="background-color: green;"
+                   #                        ,icon = icon("binoculars")))
+                   #          , div(id="test_div", sliderInput("df_rows",label=NA,1,1000,value=100,ticks = FALSE))
+                   #          , downloadButton("downloadData","Download",style="float:right;"))
+                 ),fluidRow(
+                    column(12,id = "var_header",column(3,"Variable Name"),column(4,"Type"),column(4,"Options / Details"))
 
                    # test creating my own infobox
                    , box(background = "black"
