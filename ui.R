fbf35b55e125ab1f936f0802220171a93bfd9b50
“ncpollock” <noah.pollock@yahoo.com>
2019-06-13 20:58:51 -0400

    fixed footer, trimmed out old code

--- ui.R
+++ ui.R
@@ -8,7 +8,17 @@ shinyUI(
                          # ,style=paste0("color: white;")
                          )
                , windowTitle = "Data Generator"
-               , footer = div(id="site-footer")
+               , footer = div(id="site-footer"
+                              , p(strong("Developed by: "),
+                                  br(),
+                                  a(href="https://ncpollock.github.io/"
+                                    ,target="_blank"
+                                    ,"Noah C. Pollock"),
+                                  br(),
+                                  a(href = "https://github.com/ncpollock/"
+                                    ,target="_blank"
+                                    ,"Code on GitHub"),
+                                  align="center"))
                , tabPanel(
                  'Generate Data', icon = icon("sliders")
                  , fluidRow(
@@ -36,11 +46,10 @@ shinyUI(
                                                     ,width = "100%"
                                                     ,style="background-color: green;"
                                                     ,icon = icon("plus-square")))
-                            , column(6,actionButton("preview_ML","Visualize Associations"
+                            , column(6,actionButton("preview_ML","Preview Association"
                                                     ,width = "100%"
                                                     ,icon = icon("bar-chart"))
                             )
-                            # , column(2,downloadButton("downloadData","Download",style="float:right;background-color: blue;"))
                  )
                  , fluidRow(
                    br()
@@ -49,37 +58,18 @@ shinyUI(
                  # use selectize input
                  # first variable with have greatest predictive power?
                  # or can selectize be ordered?
-                 , h2(id = "no_assoc","There are no Variable Associations defined yet.")
+                 , h2(id = "no_assoc","There are no Variable Associations defined yet.",style="align:center;color:gray;")
                  
                  # retain for testing
-                 , tableOutput("show_inputs")
+                 # , tableOutput("show_inputs")
                  )
                
-               # , tabPanel("Variable Associations", icon = icon("chain")
-               #            , fluidRow(id="GlobalTools_ML",class="sticky"
-               #                       , column(6,actionButton("add_ML","Add New Association"
-               #                                               ,width = "100%"
-               #                                               ,style="background-color: green;"
-               #                                               ,icon = icon("plus-square")))
-               #                       , column(6,actionButton("preview_ML","Visualize Associations"
-               #                                               ,width = "100%"
-               #                                               ,icon = icon("bar-chart"))
-               #                       )
-               #                       # , column(2,downloadButton("downloadData","Download",style="float:right;background-color: blue;"))
-               #            )
-               #            , fluidRow(
-               #              br()
-               #              , column(12,id = "var_header_ML",column(3,"Predictor(s)"),column(4,"Outcome"),column(4,"Options / Details"))
-               #            )
-               #            # use selectize input
-               #            # first variable with have greatest predictive power?
-               #              # or can selectize be ordered?
-               #            , h2(id = "no_assoc","There are no Variable Associations defined yet.")
-               #  )
-               
                , tabPanel('About/Help', icon = icon("question-circle")
+                          , div(class= "padded_page"
                           , h2("What Is Fake Data and Why Generate It?")
-                            , p("Although real data is increasingly easier to obtain (e.g., data.gov, kaggle.com), there are times when
+                            , p("On this site, fake data is data that looks real but has been fabricated rather than observed/recorded. 
+                                It does not accurately represent any person, place, or other entity that it mimics. 
+                                Although real data is increasingly easier to obtain (e.g., data.gov, kaggle.com), there are times when
                                 you may need a dataset with very specific attributes. This app was inspired by an onslought of course projects
                                 that, in order to begin the relevant work, required finding a suitable dataset first.")
                           , h4("A few good reasons to create your own data:")
@@ -102,20 +92,10 @@ shinyUI(
                            , tags$li("https://ebsubudhi.shinyapps.io/DataGeneration/")
                            , tags$li("https://www.onlinedatagenerator.com/")
                              )
-                        ,p(strong("Developed by: "),
-                           br(),
-                           a(href="https://ncpollock.github.io/"
-                             ,target="_blank"
-                             ,"Noah C. Pollock"),
-                           br(),
-                           a(href = "https://github.com/ncpollock/"
-                             ,target="_blank"
-                             ,"Code on GitHub"),
-                           align="center")
                         , tags$script( # adds my name in navbar
                           HTML(
                             paste0("var header = $('.navbar> .container-fluid');header.append('"
                                    , my_navbar_info,"');console.log(header)"))))
-               
+               )
                
                )))
